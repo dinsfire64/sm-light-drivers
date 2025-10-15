319c9e2ca50977e8a3674e042e843763ba3fc877
din <dinsfire64@gmail.com>
2025-10-14 21:10:19 -0500

    move parallel lights to lightsman

--- driver_parallel_lights_io/src/parallel_lights_io.cpp
+++ driver_parallel_lights_io/src/parallel_lights_io.cpp
@@ -1,165 +1,144 @@
 #define WIN32_LEAN_AND_MEAN
 
+#include <LightsManager.h>
+
 #include "parallel_lights_io.h"
-#include <unilight.h>
 
 #define PORT_LPT1 0x378
 #define PORT_LPT2 0x278
 #define PORT_LPT3 0x3BC
 
-#define BD_UNILIGHT_LIGHT_PAD_P1_ALL ( \
-  BD_UNILIGHT_LIGHT_PAD_P1_LEFT  | \
-  BD_UNILIGHT_LIGHT_PAD_P1_RIGHT | \
-  BD_UNILIGHT_LIGHT_PAD_P1_UP    | \
-  BD_UNILIGHT_LIGHT_PAD_P1_DOWN)
-
-#define BD_UNILIGHT_LIGHT_PAD_P2_ALL ( \
-  BD_UNILIGHT_LIGHT_PAD_P2_LEFT  | \
-  BD_UNILIGHT_LIGHT_PAD_P2_RIGHT | \
-  BD_UNILIGHT_LIGHT_PAD_P2_UP    | \
-  BD_UNILIGHT_LIGHT_PAD_P2_DOWN)
-
 #define IS_BIT_SET(data, bit) ((data & (1 << bit)) != 0)
 
-static uint8_t g_cab_lights_state = 0;
-static uint8_t g_pad_lights_state = 0;
+LightsManager lm;
+LightsState light_state;
 
 BOOLEAN WINAPI DllMain(IN HINSTANCE hDllHandle, IN DWORD nReason, IN LPVOID Reserved)
 {
-  switch (nReason) {
+  switch (nReason)
+  {
   case DLL_PROCESS_ATTACH:
-    if(!bd_unilight_is_open()) {
-      if(bd_unilight_open() != 0) {
-        return FALSE;
-      }
+    if (!lm.IsConnected())
+    {
+      lm.Initialize();
     }
     break;
   case DLL_PROCESS_DETACH:
-    if(bd_unilight_is_open()) {
-      bd_unilight_close();
-    }
     break;
   default:
-      break;
+    break;
   }
   return TRUE;
 }
 
-void WriteCabLights() {
-  if(bd_unilight_set_cab_lights(g_cab_lights_state) != 0) {
-    // No error handling in this implementation
-  }
+void SetCabLights(char Data)
+{
+  light_state.marquee_up_left = IS_BIT_SET(Data, 0);
+  light_state.marquee_up_right = IS_BIT_SET(Data, 1);
+  light_state.marquee_lr_left = IS_BIT_SET(Data, 2);
+  light_state.marquee_lr_right = IS_BIT_SET(Data, 3);
+  light_state.p1_menu = IS_BIT_SET(Data, 4);
+  light_state.p2_menu = IS_BIT_SET(Data, 5);
+  light_state.bass = IS_BIT_SET(Data, 6);
 }
 
-void WritePadLights() {
-  if(bd_unilight_set_pad_lights(g_pad_lights_state) != 0) {
-    // No error handling in this implementation
-  }
+void SetPad1Lights(char Data)
+{
+  light_state.p1_up = IS_BIT_SET(Data, 0);
+  light_state.p1_down = IS_BIT_SET(Data, 1);
+  light_state.p1_left = IS_BIT_SET(Data, 2);
+  light_state.p1_right = IS_BIT_SET(Data, 3);
 }
 
-void SetCabLights(char Data) {
-  g_cab_lights_state = 0;
-  if(IS_BIT_SET(Data, 0)) g_cab_lights_state |= BD_UNILIGHT_LIGHT_CAB_MARQUEE_UP_LEFT;
-  if(IS_BIT_SET(Data, 1)) g_cab_lights_state |= BD_UNILIGHT_LIGHT_CAB_MARQUEE_UP_RIGHT;
-  if(IS_BIT_SET(Data, 2)) g_cab_lights_state |= BD_UNILIGHT_LIGHT_CAB_MARQUEE_LR_LEFT;
-  if(IS_BIT_SET(Data, 3)) g_cab_lights_state |= BD_UNILIGHT_LIGHT_CAB_MARQUEE_LR_RIGHT;
-  if(IS_BIT_SET(Data, 4)) g_cab_lights_state |= BD_UNILIGHT_LIGHT_CAB_BUTTONS_P1;
-  if(IS_BIT_SET(Data, 5)) g_cab_lights_state |= BD_UNILIGHT_LIGHT_CAB_BUTTONS_P2;
-  if(IS_BIT_SET(Data, 6)) g_cab_lights_state |= BD_UNILIGHT_LIGHT_CAB_BASS;
-  if(IS_BIT_SET(Data, 7)) g_cab_lights_state |= BD_UNILIGHT_LIGHT_CAB_BASS;
+void SetPad2Lights(char Data)
+{
+  light_state.p2_up = IS_BIT_SET(Data, 0);
+  light_state.p2_down = IS_BIT_SET(Data, 1);
+  light_state.p2_left = IS_BIT_SET(Data, 2);
+  light_state.p2_right = IS_BIT_SET(Data, 3);
 }
 
-void SetPad1Lights(char Data) {
-  g_pad_lights_state &= ~(BD_UNILIGHT_LIGHT_PAD_P1_ALL);
-  if(IS_BIT_SET(Data, 0)) g_pad_lights_state |= BD_UNILIGHT_LIGHT_PAD_P1_LEFT;
-  if(IS_BIT_SET(Data, 1)) g_pad_lights_state |= BD_UNILIGHT_LIGHT_PAD_P1_RIGHT;
-  if(IS_BIT_SET(Data, 2)) g_pad_lights_state |= BD_UNILIGHT_LIGHT_PAD_P1_UP;
-  if(IS_BIT_SET(Data, 3)) g_pad_lights_state |= BD_UNILIGHT_LIGHT_PAD_P1_DOWN;
-}
-
-void SetPad2Lights(char Data) {
-  g_pad_lights_state &= ~(BD_UNILIGHT_LIGHT_PAD_P2_ALL);
-  if(IS_BIT_SET(Data, 0)) g_pad_lights_state |= BD_UNILIGHT_LIGHT_PAD_P2_LEFT;
-  if(IS_BIT_SET(Data, 1)) g_pad_lights_state |= BD_UNILIGHT_LIGHT_PAD_P2_RIGHT;
-  if(IS_BIT_SET(Data, 2)) g_pad_lights_state |= BD_UNILIGHT_LIGHT_PAD_P2_UP;
-  if(IS_BIT_SET(Data, 3)) g_pad_lights_state |= BD_UNILIGHT_LIGHT_PAD_P2_DOWN;
-}
-
-short int WINAPI IsDriverInstalled(){
-    
-    return 1;
+short int WINAPI IsDriverInstalled()
+{
+  return 1;
 }
 
 void WINAPI PortOut(short int port, char data)
 {
+  switch (port)
+  {
+  case SHRT_MAX:
+  case PORT_LPT1:
+    SetCabLights(data);
+    break;
+  case PORT_LPT2:
+    SetPad1Lights(data);
+    break;
+  case PORT_LPT3:
+    SetPad2Lights(data);
 
-  switch(port) {
-    case PORT_LPT1:
-      SetCabLights(data);
-      WriteCabLights();
-      break;
-    case PORT_LPT2:
-      SetPad1Lights(data);
-      break;
-    case PORT_LPT3:
-      SetPad2Lights(data);
-      WritePadLights(); // Write only after both pads are set.
-      break;
-    default:
-      break;
+    // Write only after everything is set.
+    lm.SetAll(&light_state);
+    break;
+  default:
+    break;
   }
 }
 
 // --- STUB IMPLEMENTATIONS FOR OTHER EXPORTED FUNCTIONS (with ) ---
 
-char WINAPI PortIn(short int port){
+char WINAPI PortIn(short int port)
+{
 
   return '\0';
 }
 
-void WINAPI ClrPortBit(short int Port, int Bit) {
-  
+void WINAPI ClrPortBit(short int Port, int Bit)
+{
 }
 
-short int WINAPI GetPortBit(short int Port, int Bit) {
-  
+short int WINAPI GetPortBit(short int Port, int Bit)
+{
+
   return 0;
 }
 
-void WINAPI LeftPortShift(short int Port, int ShiftAmount) {
-  
+void WINAPI LeftPortShift(short int Port, int ShiftAmount)
+{
 }
 
-void WINAPI NotPortBit(short int Port, int Bit) {
-  
+void WINAPI NotPortBit(short int Port, int Bit)
+{
 }
 
-unsigned long WINAPI PortDWordIn(short int Port) {
-  
+unsigned long WINAPI PortDWordIn(short int Port)
+{
+
   return 0;
 }
 
-void WINAPI PortDWordOut(short int Port, unsigned long Data) {
-  
+void WINAPI PortDWordOut(short int Port, unsigned long Data)
+{
 }
 
-short int WINAPI PortWordIn(short int Port) {
-  
+short int WINAPI PortWordIn(short int Port)
+{
+
   return 0;
 }
 
-void WINAPI PortWordOut(short int Port, short int Data) {
-  
+void WINAPI PortWordOut(short int Port, short int Data)
+{
 }
 
-void WINAPI ReleasePort(short int Port) {
-  
+void WINAPI ReleasePort(short int Port)
+{
 }
 
-void WINAPI RightPortShift(short int Port, int ShiftAmount) {
-  
+void WINAPI RightPortShift(short int Port, int ShiftAmount)
+{
 }
 
-void WINAPI SetPortBit(short int Port, int Bit) {
-  
+void WINAPI SetPortBit(short int Port, int Bit)
+{
 }
