fee87fd429c31c3d467bf35300925d92ae91f434
din <dinsfire64@gmail.com>
2025-10-16 17:32:02 -0500

    pacdrive order fix

--- driver_pacdrive/src/pacdrive.cpp
+++ driver_pacdrive/src/pacdrive.cpp
@@ -33,14 +33,16 @@ bool PacSetLEDStates(int deviceId, short int data)
 {
   LightsState ls;
 
-  ls.p1_up = IS_BIT_SET(data, 0);
-  ls.p1_down = IS_BIT_SET(data, 1);
-  ls.p1_left = IS_BIT_SET(data, 2);
-  ls.p1_right = IS_BIT_SET(data, 3);
-  ls.p2_up = IS_BIT_SET(data, 4);
-  ls.p2_down = IS_BIT_SET(data, 5);
-  ls.p2_left = IS_BIT_SET(data, 6);
-  ls.p2_right = IS_BIT_SET(data, 7);
+  // this is the correct "openITG" order of lights
+
+  ls.p1_left = IS_BIT_SET(data, 0);
+  ls.p1_right = IS_BIT_SET(data, 1);
+  ls.p1_up = IS_BIT_SET(data, 2);
+  ls.p1_down = IS_BIT_SET(data, 3);
+  ls.p2_left = IS_BIT_SET(data, 4);
+  ls.p2_right = IS_BIT_SET(data, 5);
+  ls.p2_up = IS_BIT_SET(data, 6);
+  ls.p2_down = IS_BIT_SET(data, 7);
 
   ls.marquee_up_left = IS_BIT_SET(data, 8);
   ls.marquee_up_right = IS_BIT_SET(data, 9);
@@ -48,7 +50,7 @@ bool PacSetLEDStates(int deviceId, short int data)
   ls.marquee_lr_right = IS_BIT_SET(data, 11);
   ls.p1_menu = IS_BIT_SET(data, 12);
   ls.p2_menu = IS_BIT_SET(data, 13);
-  ls.bass = IS_BIT_SET(data, 14);
+  ls.bass = IS_BIT_SET(data, 14) || IS_BIT_SET(data, 15);
 
   lm.SetAll(&ls);
 
